01b13ba6ba10a7e67b3b27bfb86e31376beda210
YitavGil <yitavga@yahoo.com>
2022-01-30 12:14:47 +0200

    first fe commit

--- Client/src/components/App/App.jsx
+++ Client/src/components/App/App.jsx
@@ -1,8 +1,31 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.scss';
+import Dropdown from '../Dropdown/Dropdown';
+
+const items = [
+  {
+    id: 1,
+    value: 'Hebrew',
+  },
+  {
+    id: 2,
+    value: 'Arabic',
+  },
+  {
+    id: 3,
+    value: 'English',
+  },
+];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>
+       Mama Lisa
+      </h1>
+      <Dropdown title="Select Language" items={items} multiSelect />
+    </div>
+  );
 }
 
 export default App;
