3607b8665a6499d327fdaa186881370f08931457
saharoori <saharlaor@gmail.com>
2022-01-30 15:03:10 +0200

    Get all songs from each language

--- Server/scraper/scraper.js
+++ Server/scraper/scraper.js
@@ -1,10 +1,10 @@
 const puppeteer = require("puppeteer");
 
 const BASE_URL = "https://mamalisa.com";
-const LANGUAGE_LIST_URI = "?t=el";
+const LANGUAGE_URI = "?t=el";
 
 async function getLanguages(page) {
-  await page.goto(`${BASE_URL}/${LANGUAGE_LIST_URI}`);
+  await page.goto(`${BASE_URL}/${LANGUAGE_URI}`);
   const languages = await page.evaluate(() => {
     const links = document.querySelectorAll("#main .row a");
     [...links].map(({ href, innerText }) => {
@@ -15,12 +15,30 @@ async function getLanguages(page) {
   return languages;
 }
 
+async function getLanguageSongs(page, languages) {
+  const songs = await languages.map(async ({ url, name }) => {
+    await page.goto(url);
+    const langSongs = await page.evaluate(() => {
+      const links = document.querySelectorAll("#main .row a");
+      [...links].map((link) => {
+        const originalTitle = link.querySelector("h2").innerText;
+        const englishTitle = link.querySelector("p").innerText;
+        return { url: link.href, originalTitle, englishTitle, language: name };
+      });
+    });
+    return langSongs;
+  });
+
+  return songs;
+}
+
 async function main() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   const languages = await getLanguages(page);
 
   // TODO: getLanguageSongs
+  const songs = await getLanguageSongs(page, languages);
 
   // TODO: getSongContent
 
